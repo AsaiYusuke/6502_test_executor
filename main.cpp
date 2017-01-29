227d5f3e2ae98666fc0b49ced8bfbba998dfbb6f
Erik Lothe <eriklothe@gmail.com>
2017-01-29 12:06:39 +0100

    refer to variables by label

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@
 
 using namespace std;
 
-#define DEFAULT_TIMEOUT 1000
+#define DEFAULT_TIMEOUT 100000
 
 
-std::map<uint16_t, uint8_t> data;
+map<uint16_t, uint8_t> data;
+map<string, uint16_t> symbol_cache;
+vector<string> symbols_file;
 char* program;
 bool passed = true;
-
 
 
 uint16_t memory_offset(uint16_t address) {
@@ -43,19 +44,37 @@ void MemoryWrite(uint16_t address, uint8_t value) {
 }
 
 void load_nes_cartridge(char* file_path) {
-    printf("loading cartridge from path: %s\n", file_path);
+    //printf("loading cartridge from path: %s\n", file_path);
     streampos size;
 
     ifstream file (file_path, ios::in|ios::binary|ios::ate);
-    
-    if (file.is_open()) {
-        size = file.tellg();
-        program = new char[size];
-        file.seekg(0, ios::beg);
-        file.read(program, size);
-        file.close();
+    if (!file.is_open()) {
+        cerr << "Unable to open NES file" << endl;
+        throw 1;
     }
-    else cout << "Unable to open file" << endl;
+    size = file.tellg();
+    program = new char[size];
+    file.seekg(0, ios::beg);
+    file.read(program, size);
+    file.close();
+}
+
+vector<string> load_file(char* path) {
+    streampos size;
+
+    ifstream file(path);
+    if (!file.is_open()) {
+        cerr << "Unable to open file: " << path << endl;
+        throw 1;
+    }
+   
+    string line;
+    vector<string> lines;
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+    file.close();
+    return lines;
 }
 
 vector<string> split(string str) {
@@ -67,27 +86,29 @@ vector<string> split(string str) {
     return words;
 }
 
-int load_start_address(char* path, string label) {
-    ifstream file(path);
-    string line;
+uint16_t find_symbol(string label) {
+    if (symbol_cache.count(label) == 1) {
+        return symbol_cache[label];
+    }
     ostringstream stream;
     stream << "sym\t\"" << label << "\",value=0x";
     string label_pattern = stream.str();
     int label_pattern_size = label_pattern.size();
-    while(getline(file, line)) {
+    for (int i=0; i<symbols_file.size(); i++) {
+        string line = symbols_file[i];
         if (line.compare(0, label_pattern_size, label_pattern) == 0) {
             int i;
             for (i=label_pattern_size; line[i] != ','; i++);
             string addr = line.substr(label_pattern_size, i - label_pattern_size);
-            cout << "found the label! addr = " << addr << endl;
-            return stoi(addr, 0, 16);
+            symbol_cache[label] = stoi(addr, 0, 16);
+            return symbol_cache[label];
         }
     }
-    cout << "didnt find start address in symbols file" << endl;
+    cerr << "didnt find start address in symbols file" << endl;
     throw 2;
 }
 
-void set_pre_conditions(vector<string> test, mos6502 *cpu, char* symbols_file) {
+void set_pre_conditions(vector<string> test, mos6502 *cpu) {
     for(int i=0; i<test.size(); i++) {
         vector<string> words = split(test[i]);
         if ("set" == words[0])
@@ -99,7 +120,7 @@ void set_pre_conditions(vector<string> test, mos6502 *cpu, char* symbols_file) {
             } else if ("Y" == words[1]) {
                 cpu->Y = stoi(words[2]);
             } else if ("mem" == words[1]) {
-                data[stoi(words[2])] = stoi(words[3]);
+                data[find_symbol(words[2])] = stoi(words[3]);
             } else {
                 cout << "does not understand set " <<  words[1] << endl;
                 throw 2;
@@ -107,7 +128,7 @@ void set_pre_conditions(vector<string> test, mos6502 *cpu, char* symbols_file) {
         }
 
         if ("start" == words[0]) {
-            cpu->pc = load_start_address(symbols_file, words[1]);
+            cpu->pc = find_symbol(words[1]);
         }
     }
 }
@@ -131,7 +152,7 @@ void assert_end_condtions(vector<string> test, mos6502 cpu) {
             } else if ("Y" == words[1]) {
                 assert_equal(stoi(words[2]), cpu.Y, "Wrong value in Y");
             } else if ("mem" == words[1]){
-                assert_equal(stoi(words[3]), data[stoi(words[2])], "Wrong value in memory");
+                assert_equal(stoi(words[3]), data[find_symbol(words[2])], "Wrong value in memory");
             } else {
                 cout << "does not understand assert " <<  words[1] << endl;
                 throw 2;
@@ -140,35 +161,32 @@ void assert_end_condtions(vector<string> test, mos6502 cpu) {
     }
 }
 
-vector<string> load_test(char* test_file) {
-    printf("loading test from path: %s\n", test_file);
-    vector<string> lines;
-    string line;
-    ifstream file(test_file);
-    int i;
-    while (getline(file, line)) {
-        lines.push_back(line);
-    }
-    printf("done\n");
-    return lines;
+void print_cpu(mos6502* cpu) {
+    printf("A=%d, X=%d, Y=%d, pc=%x\n", cpu->A, cpu->X, cpu->Y, cpu->pc);
 }
 
 int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cerr << "usage: " << argv[0] << " <executable> <symbols> <test>" << endl;
+        return -1;
+    }
     load_nes_cartridge(argv[1]);
-    vector<string> test = load_test(argv[3]);
+    symbols_file = load_file(argv[2]);
+    vector<string> test = load_file(argv[3]);
 
     mos6502 cpu(MemoryRead, MemoryWrite);
 
-    set_pre_conditions(test, &cpu, argv[2]);
-    printf("A=%d, X=%d, Y=%d\n", cpu.A, cpu.X, cpu.Y);
+    set_pre_conditions(test, &cpu);
+    print_cpu(&cpu);
 
     int count = 0;
     uint16_t pc_start = cpu.pc;
-
+    printf("starting\n");
     do {
         cpu.Run(1);
+        printf("pc = %x\n", cpu.pc);
     }
-    while ((cpu.pc <= pc_start || cpu.pc > pc_start + 3) && count++ < DEFAULT_TIMEOUT);
+    while (!(cpu.pc > pc_start && cpu.pc <= pc_start + 3) && ++count < DEFAULT_TIMEOUT);
 
     if (count == DEFAULT_TIMEOUT) {
         printf("Stopped after timed out\n");
@@ -176,6 +194,7 @@ int main(int argc, char* argv[]) {
     } else {
         printf("Stopped upon reaching PC=%x\n", cpu.pc);
     }
+    print_cpu(&cpu);
 
     printf("Ran %d instructions\n", count);
 
