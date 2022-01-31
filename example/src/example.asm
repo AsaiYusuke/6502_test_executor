4a67a2dc2ac235e4bf90e748f89d475485c94c52
Yusuke Asai <prometric2000@gmail.com>
2022-01-30 18:06:43 -0800

    update example

--- example/src/example.asm
+++ example/src/example.asm
@@ -1,7 +1,5 @@
 .include	"nes.inc"
 
-.define		SPRITE_SPEED_X	6
-.define		SPRITE_SPEED_Y	8
 .define		BORDER_MIN		32
 .define		BORDER_MAX		224
 
@@ -22,16 +20,33 @@
 	.endrepeat
 
 .segment "OAM"
-oam:		.res 256
+oam:			.res 256
 
 .segment "ZEROPAGE"
-pos:		.res 2
-old_pos:	.res 2
-vector:		.res 2
-nmi_lock:	.res 1
+pos:			.res 2
+old_pos:		.res 2
+nmi_lock:		.res 1
+vector_addr:	.res 1
 
 .segment "RODATA"
-palette:	.byte $0F, $11, $21, $31
+.define M(val) 		.lobyte(val * -1)
+.define VADDR(val)	.lobyte(val - vector_dic)
+
+vector_dic:
+vector_dic_00:	.byte	6,		0,		VADDR(vector_dic_24),	VADDR(vector_dic_00)
+vector_dic_04:	.byte	5,		3,		VADDR(vector_dic_20),	VADDR(vector_dic_44)
+vector_dic_08:	.byte	3,		5,		VADDR(vector_dic_16),	VADDR(vector_dic_40)
+vector_dic_12:	.byte	0,		6,		VADDR(vector_dic_12),	VADDR(vector_dic_36)
+vector_dic_16:	.byte	M(3),	5,		VADDR(vector_dic_08),	VADDR(vector_dic_32)
+vector_dic_20:	.byte	M(5),	3,		VADDR(vector_dic_04),	VADDR(vector_dic_28)
+vector_dic_24:	.byte	M(6),	0,		VADDR(vector_dic_00),	VADDR(vector_dic_24)
+vector_dic_28:	.byte	M(5),	M(3),	VADDR(vector_dic_44),	VADDR(vector_dic_20)
+vector_dic_32:	.byte	M(3),	M(5),	VADDR(vector_dic_40),	VADDR(vector_dic_16)
+vector_dic_36:	.byte	0,		M(6),	VADDR(vector_dic_36),	VADDR(vector_dic_12)
+vector_dic_40:	.byte	3,		M(5),	VADDR(vector_dic_32),	VADDR(vector_dic_08)
+vector_dic_44:	.byte	5,		M(3),	VADDR(vector_dic_28),	VADDR(vector_dic_04)
+
+palette:		.byte $0F, $11, $21, $31
 
 .segment "CODE"
 .proc	reset
@@ -77,10 +92,8 @@ palette:	.byte $0F, $11, $21, $31
 	lda #128
 	sta pos + 0
 	sta pos + 1
-	lda #SPRITE_SPEED_Y
-	sta vector + 0
-	lda #SPRITE_SPEED_X
-	sta vector + 1
+	lda #0
+	sta vector_addr
 
 	lda #%10000000
 	sta PPU_CTRL1
@@ -88,7 +101,39 @@ palette:	.byte $0F, $11, $21, $31
 	sta PPU_CTRL2
 
 	:
+	jsr control
 	jmp :-
+.endproc
+
+.proc	control
+	not_pushed:
+	jsr read_joypad_a_key
+	bcc	not_pushed
+
+	pushing:
+	jsr read_joypad_a_key
+	bcs	pushing
+
+	lda vector_addr
+	clc
+	adc #4
+	cmp #VADDR(vector_dic_44) + 1
+	bmi save_id
+	lda #0
+	save_id:
+
+	sta vector_addr
+	rts
+.endproc
+
+.proc	read_joypad_a_key
+	lda #1
+	sta APU_PAD1
+	lda #0
+	sta APU_PAD1
+	lda APU_PAD1
+	lsr
+	rts
 .endproc
 
 .proc	nmi
@@ -115,20 +160,21 @@ palette:	.byte $0F, $11, $21, $31
 	jsr move_pos
 	jsr check_collision
 	lda pos + 0
-	sta oam + 0
-	lda pos + 1
 	sta oam + 3
+	lda pos + 1
+	sta oam + 0
 	lda #.hibyte(oam)
 	sta APU_SPR_DMA
 	rts
 .endproc
 
 .proc	move_pos
+	ldx vector_addr
 	.repeat 2, index
 		lda pos + index
 		sta old_pos + index
 		clc
-		adc vector + index
+		adc vector_dic + index, x
 		sta pos + index
 	.endrepeat
 	rts
@@ -137,13 +183,9 @@ palette:	.byte $0F, $11, $21, $31
 .proc	check_collision
 	.repeat 2, index
 		.scope
-			.if(index = 0)
-				SPEED_AXIS = SPRITE_SPEED_Y
-			.else
-				SPEED_AXIS = SPRITE_SPEED_X
-			.endif
+			ldx vector_addr
 
-			lda vector + index
+			lda vector_dic + index, x
 			bpl move_plus
 
 			lda #BORDER_MIN
@@ -151,20 +193,20 @@ palette:	.byte $0F, $11, $21, $31
 			bmi end_check
 			cmp old_pos + index
 			bpl end_check
-			lda #SPEED_AXIS
-			sta vector + index
-			jmp end_check
+			jmp turn
 
 			move_plus:
 			lda #BORDER_MAX
 			cmp pos + index
-			beq turn
+			beq check_old_pos
 			bpl end_check
-			turn:
+			check_old_pos:
 			cmp old_pos + index
 			bmi end_check
-			lda #.lobyte(SPEED_AXIS * -1)
-			sta vector + index
+
+			turn:
+			lda vector_dic + 2 + index, x
+			sta vector_addr
 
 			end_check:
 		.endscope
