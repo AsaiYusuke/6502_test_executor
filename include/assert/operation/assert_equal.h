cb400bafb8db9c7e6e5e21759a1b778399728992
Yusuke Asai <prometric2000@gmail.com>
2022-02-10 10:16:08 -0800

    refactoring

--- include/assert/operation/assert_equal.h
+++ include/assert/operation/assert_equal.h
@@ -11,16 +11,13 @@ class assert_equal
 {
 public:
     template <typename T>
-    static bool test(T expected, T actual, string message, vector<string> &errors);
-};
-
-template <typename T>
-bool assert_equal::test(T expected, T actual, string message, vector<string> &errors)
-{
-    if (expected != actual)
+    static bool test(T expected, T actual, string message, vector<string> &errors)
     {
-        errors.push_back(message + ": Expected: " + to_string(expected) + ", Actual: " + to_string(actual));
-        return false;
+        if (expected != actual)
+        {
+            errors.push_back(message + ": Expected: " + to_string(expected) + ", Actual: " + to_string(actual));
+            return false;
+        }
+        return true;
     }
-    return true;
-}
+};
