2c25ea12cfcbfeccd1141eb56f28fc72f9b044cd
Yusuke Asai <prometric2000@gmail.com>
2022-02-14 11:21:29 -0800

    refactoring

--- src/test_setup.cpp
+++ src/test_setup.cpp
@@ -13,10 +13,10 @@ void test_setup::execute()
     for (auto register_def : get_register_defs())
         cpu_dev->set_register(register_def.get_type(), register_def.get_value());
 
-    uint8_t status;
+    uint8_t status_bits = 0;
     for (auto status_flag_def : get_status_flag_defs())
-        status |= ((uint8_t)status_flag_def.get_type() * status_flag_def.get_value());
-    cpu_dev->set_register(register_type::P, status);
+        status_bits |= ((uint8_t)status_flag_def.get_type() * status_flag_def.get_value());
+    cpu_dev->set_register(register_type::P, status_bits);
 
     memory_device *mem_dev = get_device()->get_memory();
     for (auto memory_def : get_memory_defs())
