90474d6332ba33a597b8656a0e14f447469cb25f
Yusuke Asai <prometric2000@gmail.com>
2022-03-14 11:08:36 +0900

    added option to disable maximum cycle check

--- include/enum/cycle_type.h
+++ include/enum/cycle_type.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bits/stdc++.h>
 #include <map>
 #include <string>
 
@@ -8,11 +9,15 @@ using namespace std;
 enum class cycle_type
 {
     // NES NTSC Vsync
-    NES_NTSC_VSYNC
+    NES_NTSC_VSYNC,
+    // Disable max cycle
+    DISABLE
 };
 
 static map<string, cycle_type> cycle_name_type_map = {
-    {"NES_NTSC_VSYNC", cycle_type::NES_NTSC_VSYNC}};
+    {"NES_NTSC_VSYNC", cycle_type::NES_NTSC_VSYNC},
+    {"DISABLE", cycle_type::DISABLE}};
 
 static map<cycle_type, uint64_t> cycle_type_value_map = {
-    {cycle_type::NES_NTSC_VSYNC, 29784}};
+    {cycle_type::NES_NTSC_VSYNC, 29784},
+    {cycle_type::DISABLE, INT_MAX}};
