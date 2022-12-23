8a74d48143bc321b7f04c3ff4d31f165cb3641eb
Asai Yusuke <AsaiYusuke@users.noreply.github.com>
2022-12-23 23:04:48 +0900

    fix device not ready result (#8)

--- include/test/test_result.h
+++ include/test/test_result.h
@@ -12,6 +12,7 @@ class test_result
 private:
     string id;
     test_result_type result_type;
+    bool device_ready;
     vector<string> errors;
 
     test_result(test_result_type type);
@@ -20,6 +21,8 @@ public:
     test_result();
     void set_id(string id);
     void add_error(string error);
+    void set_device_not_ready();
+    bool is_device_ready();
     test_result_type get_result_type();
     string get_result_name();
     vector<string> get_errors();
